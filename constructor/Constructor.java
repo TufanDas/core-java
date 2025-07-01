6291db6457d410e1282f46f9c1438943f2c6a1f7
Tufan <dtufan38@gmail.com>
2025-07-01 15:07:53 +0530

    constructor

--- constructor/Constructor.java
+++ constructor/Constructor.java
@@ -1,4 +1,27 @@
 package constructor;
 
 public class Constructor {
+
+    public static void main(String args[]) {
+
+        // Creating student1 using the default constructor
+        Student student1  = new Student();
+        System.out.println("The default name of student1 is: " + student1.getName()); // name will be null
+        System.out.println("The default age of student1 is: " + student1.getAge());   // age = 10 (set by constructor)
+        System.out.println("The default Roll Number of student1 is: " + student1.getRollNumber()); // default = 0
+        System.out.println();
+
+        // Creating student2 using the full-arguments constructor
+        Student student2 = new Student("Tufan Das", 22, 44);
+        System.out.println("The name of student2 is: " + student2.getName());
+        System.out.println("The age of student2 is: " + student2.getAge());
+        System.out.println("The Roll Number of student2 is: " + student2.getRollNumber());
+        System.out.println();
+
+        // Creating student3 with only the name constructor
+        Student student3 = new Student("Tufan Das");
+        System.out.println("The name of student3 is: " + student3.getName());
+        System.out.println("The age of student3 is: " + student3.getAge()); // will be 0
+        System.out.println("The Roll Number of student3 is: " + student3.getRollNumber()); // will also be 0
+    }
 }
--- constructor/Student.java
+++ constructor/Student.java
@@ -1,4 +1,61 @@
 package constructor;
 
-public class Student {
+class Student {
+
+    // Instance variables to hold student data
+    private String name;
+    private int rollNumber;
+    private int age;
+
+    // Default constructor – sets a default age
+    public Student() {
+        this.age = 10; // Age is set to 10 if no value is provided
+    }
+
+    // Constructor with all properties – allows full initialization
+    public Student(String name, int age, int rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+
+    // Constructor with only name – other fields remain uninitialized (age, rollNumber will be 0)
+    public Student(String name) {
+        this.name = name;
+        // age and rollNumber will use their default values (0)
+    }
+
+    // Setter for age with basic validation
+    public void setAge(int x) {
+        if (x < 0) {
+            System.out.println("Invalid age, please enter a valid age.");
+        } else {
+            this.age = x;
+        }
+    }
+
+    // Getter for age
+    public int getAge() {
+        return this.age;
+    }
+
+    // Setter for  name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return this.name;
+    }
+
+    // Setter for roll number
+    public void setRollNumber(int roll) {
+        this.rollNumber = roll;
+    }
+
+    // Getter for roll number
+    public int getRollNumber() {
+        return this.rollNumber;
+    }
 }
