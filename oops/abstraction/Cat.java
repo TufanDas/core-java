f78fee5dbd0a151bcebca509a8da61e956c30f37
Tufan <dtufan38@gmail.com>
2025-07-01 14:51:19 +0530

    abstraction

--- oops/abstraction/Cat.java
+++ oops/abstraction/Cat.java
@@ -1,4 +1,12 @@
 package oops.abstraction;
 
-public class Cat {
+
+// Cat class also extends Animal and overrides sayHello
+class Cat extends Animal {
+
+    // Cat-specific behavior for sayHello
+    @Override
+    public void sayHello() {
+        System.out.println("Meow");
+    }
 }
