7697b327653b7d9c431c92ff2d5e5e1a6180fea7
Tufan <dtufan38@gmail.com>
2025-07-12 12:38:59 +0530

    Remove tracked files that should be ignored

--- innerClasses/anonymousInnerClass/withoutAnonymousClass/Payment.java
+++
@@ -1,9 +0,0 @@
-package innerClasses.anonymousInnerClass.withoutAnonymousClass;
-
-// Interface representing a generic payment method
-public interface Payment {
-
-    // Method to perform a payment of a given amount
-    // Any class implementing this interface must provide the logic for this method
-    void pay(double amount);
-}
