0b2d906c55cfa77eb5098902c8b16a68baea3c28
Tufan <dtufan38@gmail.com>
2025-07-19 20:38:25 +0530

    Basic Math functions

---
+++ Math/Test.java
@@ -0,0 +1,17 @@
+package Math;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Max: " + Math.max(a, b));   //   Max: 20
+        System.out.println("Min: " + Math.min(a, b));   //   Min: 10
+        System.out.println("Square Root of 25: " + Math.sqrt(25));  //   Square Root of 25: 5.0
+        System.out.println("2 raised to 3: " + Math.pow(2, 3)); //   2 raised to 3: 8.0
+        System.out.println("Absolute of -15: " + Math.abs(-15));  //   Absolute of -15: 15
+        System.out.println("Rounded 2.7: " + Math.round(2.7));  //   Rounded 2.7: 3
+        System.out.println("Random number: " + Math.random());  //   Random number: 0.9128815661497021
+    }
+
+}
